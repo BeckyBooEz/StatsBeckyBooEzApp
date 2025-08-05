2814dc82b771954eed3a8beed811b12501648d5c
ArturoMirandaEz <arturmiranda917@gmail.com>
2025-08-05 01:25:06 -0600

    Agregar nuevos íconos SVG para redes sociales y mejorar la estructura del encabezado y estilos en CSS

--- public/script.js
+++ public/script.js
@@ -113,20 +113,20 @@ async function obtenerTop() {
     };
 };
 
-async function obteneruser(){
+async function obteneruser() {
     try {
         const response = await fetch("/api/user/name");
 
         const data = await response.json()
 
-        const nombreuser= document.getElementById("user-name");
+        const nombreuser = document.getElementById("user-name");
         nombreuser.textContent = data.display_name
         const imguser = document.getElementById("user-image")
         imguser.src = data.images[0].url
         const usertype = document.getElementById("user-type");
         const usertypemayus = data.product.charAt(0).toUpperCase() + data.product.slice(1).toLowerCase();
         usertype.textContent = usertypemayus;
-        
+
     } catch (error) {
         console.log("No se pudo cargar user:", error)
     }
@@ -333,6 +333,8 @@ function calcularTiempo(tracks) {
 
     return `hace ${partes.slice(0, 2).join(" y ")}`;
 }
-ObtenerCanciones();
-recentplayed();
+
+obteneruser()
+ObtenerCanciones()
 obtenerTop();
+recentplayed();
